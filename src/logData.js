5ec73f5c210fba34eec2cfac444a670cdf4e4bb8
Coolmes833 <berkaykoseoglu833@gmail.com>
2025-07-14 17:42:09 +0300

    changes

---
+++ src/logData.js
@@ -0,0 +1,64 @@
+const logs = [
+    {
+        timestamp: '2025-07-14 12:01:23',
+        sourceIP: '192.168.1.10',
+        destIP: '10.0.0.5',
+        ML_classification: 'Normal',
+    },
+    {
+        timestamp: '2025-07-14 12:03:44',
+        sourceIP: '172.16.0.2',
+        destIP: '10.0.0.8',
+        ML_classification: 'Attack',
+    },
+    {
+        timestamp: '2025-07-14 12:05:12',
+        sourceIP: '192.168.0.25',
+        destIP: '10.0.0.13',
+        ML_classification: 'Anomaly',
+    },
+    {
+        timestamp: '2025-07-14 12:06:55',
+        sourceIP: '10.1.1.1',
+        destIP: '172.16.5.9',
+        ML_classification: 'Normal',
+    },
+    {
+        timestamp: '2025-07-14 12:09:20',
+        sourceIP: '192.168.1.35',
+        destIP: '10.0.0.22',
+        ML_classification: 'Attack',
+    },
+    {
+        timestamp: '2025-07-14 12:10:45',
+        sourceIP: '172.16.0.11',
+        destIP: '10.0.0.4',
+        ML_classification: 'Anomaly',
+    },
+    {
+        timestamp: '2025-07-14 12:12:38',
+        sourceIP: '192.168.1.100',
+        destIP: '10.0.0.7',
+        ML_classification: 'Normal',
+    },
+    {
+        timestamp: '2025-07-14 12:14:03',
+        sourceIP: '10.10.10.2',
+        destIP: '172.16.4.1',
+        ML_classification: 'Attack',
+    },
+    {
+        timestamp: '2025-07-14 12:15:59',
+        sourceIP: '192.168.0.50',
+        destIP: '10.0.0.18',
+        ML_classification: 'Normal',
+    },
+    {
+        timestamp: '2025-07-14 12:17:25',
+        sourceIP: '172.16.2.5',
+        destIP: '10.0.0.30',
+        ML_classification: 'Anomaly',
+    },
+];
+
+export default logs;
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import MapComponent from '../components/MapComponent'
-import { Pie } from 'react-chartjs-2';
+import { Pie, Bar } from 'react-chartjs-2';
 import 'chart.js/auto';
-
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels'; // pie chart üzerine sayısal değer için
+import logs from '../logData' // timestamp tablosu için logData.js içinde fake üretildi.
+ChartJS.register(ArcElement, Tooltip, Legend, ChartDataLabels); // plug in , grafik üstüne % ve yazı için
 
 
 // ilk harfi "D" küçük olursa html zannetti!, ilk harf componentler için daima BÜYÜK! 
@@ -10,7 +13,7 @@ import 'chart.js/auto';
 const Dashboard = () => {
     return (
         <div>
-            <nav className='flex items-center justify-between bg-gray-800 px-6 py-4'>
+            <nav className='flex items-center justify-center bg-gray-800 px-10 py-5'>
                 <div className='flex items-center space-x-4'>
                     <img
                         src="/sulh_logo.ico"
@@ -18,14 +21,20 @@ const Dashboard = () => {
                         className="h-10 w-10"
 
                     />
-                    <span className='text-xl font-bold text-gray-100'>SULH Dashboard</span>
+                    <span className='text-3xl font-bold'>SULH Dashboard</span>
                 </div>
             </nav>
 
             {/* Dashboard içerik */}
-            <div className="p-8">
-                {/* Başlık */}
-                <h1 className="text-3xl font-bold mb-8">SIEM</h1>
+            <div className="p-8 bg-black">
+                <div className="bg-gray-800 min-w-full py-2 mb-6 rounded-lg ">
+                    <nav className="flex items-center justify-between bg-gray-800 px-6 py-3 shadow">
+                        <div className="flex items-center rounded-lg space-x-6 pb-25">
+                            <span className="text-3xl font-semibold text-white">SIEM</span>
+                        </div>
+                    </nav>
+                </div>
+                {/* ÜST GRID */}
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-8">
 
                     {/* GeoIP Map */}
@@ -40,11 +49,11 @@ const Dashboard = () => {
                         <div className="h-[330px] mb-5">
                             <Pie
                                 data={{
-                                    labels: ['A', 'B', 'C'],
+                                    labels: ['Attack', 'Normal', 'Anomaly'],
                                     datasets: [
                                         {
-                                            data: [10, 20, 30],
-                                            backgroundColor: ['#f00', '#0f0', '#00f'],
+                                            data: [19.5, 11, 55.6],
+                                            backgroundColor: ['#EF4A3C	', '#F4B53F', '#1E7774'],
                                         },
                                     ],
                                 }}
@@ -56,7 +65,14 @@ const Dashboard = () => {
                                             labels: { color: '#fff', padding: 20 },
 
                                         },
-
+                                        datalabels: {
+                                            color: '#fff',
+                                            formatter: (value) => `${value.toFixed(1)}%`,
+                                            font: {
+                                                weight: 'bold',
+                                                size: 14,
+                                            },
+                                        }
                                     },
                                 }}
 
@@ -64,7 +80,83 @@ const Dashboard = () => {
                         </div>
 
                     </div>
+
+                    {/* Accuracy Bar Chart */}
+                    <div className="bg-gray-800 p-6 rounded-lg shadow h-[400px]">
+                        <h3 className='text-lg font-semibold mb-4'> Accuracy </h3>
+                        <Bar
+                            data={{
+                                labels: ['Random Forest', 'XGBoost', 'CNN'],
+                                datasets: [
+                                    {
+                                        data: [72.4, 61.3, 45.8],
+                                        backgroundColor: ['#1E7774'],
+                                    },
+                                ],
+                            }} options={{
+                                indexAxis: 'y',
+                                maintainAspectRatio: false,
+                                plugins: {
+                                    legend: { display: false },
+                                    datalabels: {
+                                        color: '#fff',
+                                        formatter: (value) => `${value.toFixed(1)}%`,
+                                        font: {
+                                            weight: 'bold',
+                                            size: 14,
+                                        },
+                                    }
+                                },
+                                scales: {
+                                    x: {
+                                        ticks: {
+                                            color: '#fff', padding: 25, font: {
+                                                size: 15,
+                                            },
+                                        },
+                                        grid: { color: '#333' },
+                                        max: 100
+                                    },
+                                    y: {
+                                        ticks: { color: '#fff' },
+                                        grid: { color: '#333' },
+                                    },
+                                },
+                            }}
+                        />
+
+                    </div>
                 </div>
+                {/* Timestamp Tablee*/}
+                <div className="bg-gray-800 p-6 rounded-lg shadow overflow-x-auto">
+                    <h3 className="text-lg font-semibold mb-4">Timestamp</h3>
+                    <table className="min-w-full text-left text-sm">
+                        <thead className="border-b border-gray-600">
+                            <tr>
+                                <th className="py-2">Timestamp</th>
+                                <th className="py-2">Source IP</th>
+                                <th className="py-2">Destination IP</th>
+                                <th className="py-2">ML Classification</th>
+                            </tr>
+                        </thead>
+                        <tbody className="divide-y divide-gray-700">
+                            {logs.map((log, index) => (
+                                <tr key={index}>
+                                    <td className="py-2">{log.timestamp}</td>
+                                    <td className="py-2">{log.sourceIP}</td>
+                                    <td className="py-2">{log.destIP}</td>
+                                    <td className="py-2">{log.ML_classification}</td>
+                                    <td
+                                    >
+                                        {log.classification}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+
+                </div>
+
 
             </div>
         </div>
